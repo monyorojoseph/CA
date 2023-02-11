868d7ca6b902be0c6f68678f769d573e412b10b6
monyorojoseph <monyorojoseph@gmail.com>
2023-02-11 11:48:49 +0300

    Web and api basic setup done

---
+++ api/user/models.py
@@ -0,0 +1,55 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django_lifecycle import LifecycleModelMixin, hook
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("User must have an email")
+        user = self.model(
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(LifecycleModelMixin, AbstractBaseUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    email = models.EmailField(max_length=50, unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name_plural = 'Users'
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
---
+++ web/pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h1>Hey looser</h1>
+    </div>
+  )
+}
